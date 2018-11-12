69b3be7eec1453d055510720b6212d93c55573b9
suresh <suresh@DESKTOP-FDD5IFB>
2018-11-12 17:09:58 +0530

    add my info details

--- src/test/testcases/MyInfo.java
+++ src/test/testcases/MyInfo.java
@@ -1,6 +1,19 @@
 package test.testcases;
 
 public class MyInfo extends UserLibrary {
+	
+	static int a=1; static int b=2;
+	static int c;
+	
+	public static void add()
+	{
+		c=a+b;
+		System.out.println(c);
+		
+	}
+	
+	
+	
 
 	
 }
